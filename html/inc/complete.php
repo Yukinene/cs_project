192d53a55f18135b3f30257e5b06dbbbc38b454b
Yukinene <pumin.s@ku.th>
2023-04-05 13:16:29 +0700

    Ver 7.2B
    Adjust alert

--- html/inc/complete.php
+++ html/inc/complete.php
@@ -1,10 +1,6 @@
 <?php
-  if (count($completes) > 0) : ?>
-  <div class="success">
-  	<?php foreach ($completes as $complete) : ?>
-		<div class="alert alert-success" role="alert">
-		สำเร็จ : <?php echo $complete ?>
-		</div>
-  	<?php endforeach ?>
-  </div>
-<?php  endif ?>
+if (count($completes) > 0) :
+  	foreach ($completes as $complete) :
+		echo "<script>alert('$complete');</script>";
+  	endforeach
+endif ?>
--- html/inc/errors.php
+++ html/inc/errors.php
@@ -1,9 +1,5 @@
-<?php  if (count($errors) > 0) : ?>
-  <div class="error">
-  	<?php foreach ($errors as $error) : ?>
-		<div class="alert alert-danger" role="alert">
-		ผิดพลาด : <?php echo $error ?>
-		</div>
-  	<?php endforeach ?>
-  </div>
-<?php  endif ?>
+<?php  if (count($errors) > 0) :
+  	foreach ($errors as $error) :
+		echo "<script>alert('$error');</script>";
+  	endforeach
+endif ?>
